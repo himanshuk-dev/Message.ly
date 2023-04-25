0f99e601ef952af831cdca8498ebfba5c2a939df
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-04-25 16:59:05 -0700

    adding user model

--- models/user.js
+++ models/user.js
@@ -1,29 +1,79 @@
 /** User class for message.ly */
 
-
+const db = require("../db");
+const ExpressError = require("../expressError");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const ExpressError = require("../expressError");
 
 /** User of the site. */
 
 class User {
-
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users (
+        username, 
+        first_name, 
+        last_name, 
+        phone, 
+        password)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id, username, first_name, last_name, phone`,
+      [username, first_name, last_name, phone, hashedPassword]
+    );
+
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT 
+      password 
+      FROM users 
+      WHERE username = $1`,
+      [username]
+    );
+
+    let user = result.rows[0];
+
+    return user && (await bcrypt.compare(password, user.password));
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+         SET last_login_at = current_timestamp
+         WHERE username = $1
+         RETURNING username`,
+      [username]
+    );
+
+    if (!result.rows[0]) {
+      throw new ExpressError(`Cannot find user: ${username}`, 404);
+    }
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone
+      FROM users
+      `
+    );
+    return result.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +84,24 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(
+      `SELECT username, 
+      first_name, 
+      last_name, 
+      phone, 
+      join_at,
+      last_login_at 
+      FROM users WHERE username = $1
+      `,
+      [username]
+    );
+    if (!result.rows[0]) {
+      throw new ExpressError(`Cannot find user: ${username}`, 404);
+    }
+
+    return result.rows[0];
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +111,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
    *
@@ -54,8 +121,35 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
-}
+  static async messagesTo(username) {
+    const result = await db.query(
+      `SELECT m.id,
+              m.from_username,
+              u.first_name,
+              u.last_name,
+              u.phone,
+              m.body,
+              m.sent_at,
+              m.read_at
+        FROM messages AS m
+         JOIN users AS u ON m.from_username = u.username
+        WHERE to_username = $1`,
+      [username]
+    );
 
+    return result.rows.map((m) => ({
+      id: m.id,
+      from_user: {
+        username: m.from_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at,
+    }));
+  }
+}
 
 module.exports = User;
